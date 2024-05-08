cf75fa08e0dc1696582b8a1cf10c1bc432a1dab8
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-05-08 13:39:58 +0900

    マウスのクリック位置から出現

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,43 +1,44 @@
 namespace BallApp {
     public partial class Form1 : Form {
-        private double posX;    //x座標
-        private double posY;    //y座標
-        private double moveX;   //移動量（x方向）
-        private double moveY;   //移動量（y方向）
+        SoccerBall soccerBall;
+        PictureBox pb;
+
+        //private double posX;    //x座標
+        //private double posY;    //y座標
+        //private double moveX;   //移動量（x方向）
+        //private double moveY;   //移動量（y方向）
 
         //コンストラクタ
         public Form1() {
             InitializeComponent();
-
-            moveX = moveY = 10;
         }
 
         //フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
-            //this.BackColor = Color.Green;
-            timer1.Start();
+            
         }
 
-        private void timer1_Tick(object sender, EventArgs e) {
-            //現在位置を表示
-            this.Text = pbBall.Location.ToString();
+        private void timer1_Tick(object sender, EventArgs e) {            
+            soccerBall.Move();
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+        }
 
-            if (pbBall.Location.X > 750 || pbBall.Location.X < 0) {
-                //移動量の符号を反転
-                moveX = -moveX;
-            }
+        private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
-            if (pbBall.Location.Y > 500 || pbBall.Location.Y < 0) {
-                //移動量の符号を反転
-                moveY = -moveY;
-            }
+            pb = new PictureBox(); //画像を表示するコントロール
+            pb.Size = new Size(50, 50);
 
-            posX += moveX;
-            posY += moveY;
+            soccerBall = new SoccerBall(e.X,e.Y);
+         
 
-            pbBall.Location = new Point((int)posX, (int)posY);
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
 
-
+            pb.Image = soccerBall.Image;
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Parent = this;
+            
+            timer1.Start();
         }
     }
 }
