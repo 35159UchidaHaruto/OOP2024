53ce6dab9ac07be63d905261e61941f748a702ae
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-05-08 14:44:29 +0900

    テニスボール作成

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,6 +1,7 @@
 namespace BallApp {
     public partial class Form1 : Form {
         SoccerBall soccerBall;
+        TennisBall tennisBall;
         PictureBox pb;
 
         //private double posX;    //x座標
@@ -18,27 +19,42 @@ namespace BallApp {
             
         }
 
-        private void timer1_Tick(object sender, EventArgs e) {            
+        private void timer1_Tick(object sender, EventArgs e) {
             soccerBall.Move();
             pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+
+            tennisBall.Move();
+            pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
             pb = new PictureBox(); //画像を表示するコントロール
-            pb.Size = new Size(50, 50);
+            pb.Size = new Size(50, 50);                 
 
-            soccerBall = new SoccerBall(e.X,e.Y);
-         
+            if (e.Button == MouseButtons.Left) { 
+                soccerBall = new SoccerBall(e.X, e.Y);
+                pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+                pb.Image = soccerBall.Image;
+                pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this;
 
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+                timer1.Start();
 
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;
-            pb.Parent = this;
+            } else if (e.Button == MouseButtons.Right) {
+                tennisBall = new TennisBall(e.X-25, e.Y-25);
+                pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
+                pb.Image = tennisBall.Image;
+                pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this;
+
+               
+
+
+            }
             
-            timer1.Start();
         }
     }
 }
