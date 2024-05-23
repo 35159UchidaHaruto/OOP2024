207c988762c3acec9fbb22f7d07929b84f777fe5
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-05-23 13:59:54 +0900

    修正

---
+++ Chapter04/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var age = 25;
+
+            var langs = new string[] { 
+                "C#", 
+                "VB", 
+                "C++",
+            };
+            var nums = new int[] { 10, 20, 30, 40, };
+
+            var dict = new Dictionary<string, string> { { } }
+
+
+        }
+    }
+}
