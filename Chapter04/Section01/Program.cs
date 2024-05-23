db06e3a6f5f41b4b96a280fc7c8f476a805d0de3
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-05-23 15:59:06 +0900

    P128まで

--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,18 +8,36 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var age = 25;
 
-            var langs = new string[] { 
-                "C#", 
-                "VB", 
-                "C++",
-            };
-            var nums = new int[] { 10, 20, 30, 40, };
+            #region null 条件演算子
 
-            var dict = new Dictionary<string, string> { { } }
+            #endregion
+            
+            Console.Write("整数を入力:");
+            string inputNum = Console.ReadLine();
+            int num;
+            if (int.TryParse(inputNum, out num)) {
+                
+                Console.WriteLine("整数に変換した値:" + num);
+            }else {
+                Console.WriteLine("変換に失敗しました。");
+            }
+            
 
 
+
+            /*private static object GetMessage(string code) {
+            return null;
+            }*/
+
+            /*private static object DefaultMessage() {
+                return "Defalut Message";
+            }*/
         }
     }
 }
+            #region null合体演算子
+            /*string code = "12345";
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);*/
+            #endregion
