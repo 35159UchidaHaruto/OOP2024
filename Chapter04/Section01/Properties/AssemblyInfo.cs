3a647b2f58d9b63f5ce65eebbddee15f5d369428
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-05-29 09:28:02 +0900

    Chapter4のプログラムを訂正

--- Chapter04/Section01/Properties/AssemblyInfo.cs
+++ Chapter04/Section01/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("fba66d88-4b8c-4dc7-9efc-3c2bb848ca55")]
+[assembly: Guid("cca9a935-c9fa-4664-8b3a-63924f8f1fa4")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
