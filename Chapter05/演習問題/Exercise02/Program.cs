ec60f6e683500c578a4efebfd608af28f5c61bd2
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-05-29 15:44:50 +0900

    5.1 完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var s = (int)Console.ReadLine();
+            
+        }
+    }
+}
