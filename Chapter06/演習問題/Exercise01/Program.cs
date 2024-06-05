9c7c4f7a0beb530a7e9bc33c0e75675a9caba102
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-06-05 15:29:46 +0900

    演習問題第6章-1番完成

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,23 +26,36 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_1(int[] numbers) {
+            var maxNum = numbers.Max();
+            Console.WriteLine(maxNum);
             
         }
 
         private static void Exercise1_2(int[] numbers) {
-            
+            var skip = numbers.Length - 2;
+            foreach(var number in numbers.Skip(skip)) {
+                Console.WriteLine(number);
+            }
         }
 
         private static void Exercise1_3(int[] numbers) {
+            var num = numbers.Select(n => n.ToString());
+            foreach (var nums in num) {
+                Console.WriteLine(nums);
+            }
             
         }
 
         private static void Exercise1_4(int[] numbers) {
-            
+            var sortedNum = numbers.OrderBy(x => x).Take(3);
+            foreach (var num in sortedNum) {
+                Console.WriteLine(num);
+            }
         }
 
         private static void Exercise1_5(int[] numbers) {
-            
+            var num = numbers.Distinct().Count(n => n > 10);           
+                Console.WriteLine(num);          
         }
     }
 }
