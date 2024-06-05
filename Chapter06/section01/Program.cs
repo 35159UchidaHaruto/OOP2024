823654d2cdd7bd61b3ce001677a643baac584b49
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-06-05 12:05:39 +0900

    第6章終わり

---
+++ Chapter06/section01/Program.cs
@@ -0,0 +1,26 @@
+﻿using Section01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks().OrderByDescending(b => b.Price).ToArray();
+
+            books.ForEach(b => Console.WriteLine(b.Title + ":" + b.Price));
+
+            /*var average = Books.GetBooks().Average(x=>x.Pages);
+            Console.WriteLine(average);
+
+            var page = Books.GetBooks().Max(x=> x.Pages);
+            Console.WriteLine(page);
+
+            var bookTitle = Books.GetBooks().FirstOrDefault(b => b.Pages == page);
+            Console.WriteLine(bookTitle.Title);*/
+        }
+    }
+}
