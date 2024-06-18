040bb3c85b52e1cc4baa59e87d5bf3cdb6a40446
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-06-18 11:23:35 +0900

    設問１_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
 
 namespace Test01 {
@@ -16,21 +17,28 @@ namespace Test01 {
             List<Student> students = new List<Student>();
             string[] lines = File.ReadAllLines(filePath);
             foreach(string line in lines) {
-                String[] 
+                string[] items = line.Split(',');
+                Student student = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
             }
-
-
-
-
+            return students;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
+            Dictionary<string, int>dict = new Dictionary<string, int>();
+            foreach(Student student in _score) {
+                if (dict.ContainsKey(student.Subject))
+                    dict[student.Subject] += student.Score;
+                else
+                    dict[student.Subject] = student.Score;
+                              
+            }
+              return dict;
         }
     }
 }
