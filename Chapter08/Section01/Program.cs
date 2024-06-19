d032ac64f5c0117c6eb9abb6e31e529f29b3dcd6
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-06-19 15:57:23 +0900

    204ページまで

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dt1 = new DateTime(2024,6,19);
+            var dt2 = new DateTime(2005, 3, 2, 12, 00, 00);
+            Console.WriteLine(dt1);
+            Console.WriteLine(dt2);
+
+
+            var today = DateTime.Today;            
+            var now = DateTime.Now;
+            Console.WriteLine("today :{0}",today);
+            Console.WriteLine("now : {0}", now);
+
+            var isLeapYear = DateTime.IsLeapYear(2000);
+            if (isLeapYear) {
+                Console.WriteLine("閏年です。");
+            }else {
+                Console.WriteLine("閏年ではありません。");
+            }
+        }
+    }
+}
